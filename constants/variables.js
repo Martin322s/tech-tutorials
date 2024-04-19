ed38131f2a634467cb98e61978712bd07bbed5db
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-19 11:15:38 +0300

    install all dependency libraries

---
+++ constants/variables.js
@@ -0,0 +1,3 @@
+export const CONNECTION_STRING = "mongodb+srv://martin13s18:01234567890Mero.@atlascluster.vbqdt4k.mongodb.net/FoodApp";
+export const SECRET = "kdjgj498u74hfghdfh7w782348ghf23f347929f82fh828fh8";
+export const SALT_ROUNDS = 10;
