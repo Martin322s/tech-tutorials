f4b2300e6998bc216c4b04ae85e27cdab5906b5f
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-21 16:13:09 +0300

    user database model

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    enrolledCourses: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
