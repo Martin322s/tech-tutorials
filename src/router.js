7864bd1365369c5c148972687be1709a1aef5d90
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-22 13:57:02 +0300

    register service

--- src/router.js
+++ src/router.js
@@ -3,14 +3,15 @@ const authController = require('./controllers/authController');
 const tutorialsController = require('./controllers/tutorialsController');
 
 router.get('/', (req, res) => {
-    res.render('home');
+    
+    res.render('home', { user: {} });
 });
 
 router.use('/auth', authController);
 router.use('/course', tutorialsController);
 
 router.get('*', (req, res) => {
-    
+
 });
 
 module.exports = router;
---
+++ src/services/authService.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const jwtSign = promisify(jwt.sign);
+const { SALT_ROUNDS, SECRET } = require('../../config/constants');
+
+exports.registerUser = async (userData) => {
+    const user = await User.findOne({ email: userData.email });
+
+    try {
+        if (!user) {
+            const salt = await bcrypt.genSalt(SALT_ROUNDS);
+            const hashedPassword = await bcrypt.hash(userData.password, salt);
+            const user = await User.create({ ...userData, password: hashedPassword });
+            return user;
+        } else {
+            throw { message: 'User with this email already exsists!' }
+        }
+    } catch (err) {
+        return err.message;
+    }
+};
