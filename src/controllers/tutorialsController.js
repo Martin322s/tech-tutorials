5248635e6a90c63b89ae56bf83764e6ea62468c6
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-29 14:15:23 +0300

    validate create course info

--- src/controllers/tutorialsController.js
+++ src/controllers/tutorialsController.js
@@ -1,7 +1,24 @@
+const { isAuth } = require('../middlewares/authMiddleware');
+const tutorialsService = require('../services/tutorialsService');
+
 const router = require('express').Router();
 
-router.get('/create', (req, res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('create-course');
+});
+
+router.post('/create', (req, res) => {
+    const { title, description, imageUrl, duration } = req.body;
+
+    try {
+        if (Object.values(req.body).some(x => x === '')) {
+            alert('All fields are required!');
+        } else {
+            
+        }
+    } catch (err) {
+
+    }
 });
 
 router.get('/details/:id', (req, res) => {
